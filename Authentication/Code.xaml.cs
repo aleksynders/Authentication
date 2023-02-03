ed5d1d1ac0cbc742b54d90e666d286c27b0b45f6
sanyogromov@mail.ru <sanyogromov@mail.ru>
2023-02-03 09:41:03 +0300

    up

---
+++ Authentication/Code.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Authentication
+{
+    /// <summary>
+    /// Логика взаимодействия для Code.xaml
+    /// </summary>
+    public partial class Code : Window
+    {
+
+        string code;
+        int time = 10;
+        DispatcherTimer disTimer = new DispatcherTimer();
+
+        public Code(string code)
+        {
+            InitializeComponent();
+            this.code = code;
+            disTimer.Interval = new TimeSpan(0, 0, 1);
+            disTimer.Tick += new EventHandler(Timer);
+            disTimer.Start();
+            MainWindow.countCode = 0;
+        }
+
+        private void Timer(object sender, EventArgs e)
+        {
+            if (time == 0)
+                this.Close();
+            else
+                tbRemainingTime.Text = "Оставшееся время: " + time + " секунд";
+            time--;
+        }
+
+        private void tbCode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbCode.Text == code)
+            {
+                MainWindow.codeWrite = true;
+                this.Close();
+            }
+            MainWindow.countCode = tbCode.Text.Length;
+        }
+    }
+}
--- Authentication/MainWindow.xaml.cs
+++ Authentication/MainWindow.xaml.cs
@@ -12,17 +12,47 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Authentication
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    ///
+    
+
+
+//
+//██╗░░░░░░█████╗░░██████╗░██╗███╗░░██╗██╗  ░█████╗░██████╗░███╗░░░███╗
+//██║░░░░░██╔══██╗██╔════╝░██║████╗░██║╚═╝  ██╔══██╗██╔══██╗████╗░████║
+//██║░░░░░██║░░██║██║░░██╗░██║██╔██╗██║░░░  ███████║██║░░██║██╔████╔██║
+//██║░░░░░██║░░██║██║░░╚██╗██║██║╚████║░░░  ██╔══██║██║░░██║██║╚██╔╝██║
+//███████╗╚█████╔╝╚██████╔╝██║██║░╚███║██╗  ██║░░██║██████╔╝██║░╚═╝░██║
+//╚══════╝░╚════╝░░╚═════╝░╚═╝╚═╝░░╚══╝╚═╝  ╚═╝░░╚═╝╚═════╝░╚═╝░░░░░╚═╝
+
+//██████╗░░█████╗░░██████╗░██████╗░██╗░░░░░░░██╗░█████╗░██████╗░██████╗░██╗  ░█████╗░██████╗░███╗░░░███╗
+//██╔══██╗██╔══██╗██╔════╝██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔══██╗╚═╝  ██╔══██╗██╔══██╗████╗░████║
+//██████╔╝███████║╚█████╗░╚█████╗░░╚██╗████╗██╔╝██║░░██║██████╔╝██║░░██║░░░  ███████║██║░░██║██╔████╔██║
+//██╔═══╝░██╔══██║░╚═══██╗░╚═══██╗░░████╔═████║░██║░░██║██╔══██╗██║░░██║░░░  ██╔══██║██║░░██║██║╚██╔╝██║
+//██║░░░░░██║░░██║██████╔╝██████╔╝░░╚██╔╝░╚██╔╝░╚█████╔╝██║░░██║██████╔╝██╗  ██║░░██║██████╔╝██║░╚═╝░██║
+//╚═╝░░░░░╚═╝░░╚═╝╚═════╝░╚═════╝░░░░╚═╝░░░╚═╝░░░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝  ╚═╝░░╚═╝╚═════╝░╚═╝░░░░░╚═╝
+//
+
+
     public partial class MainWindow : Window
     {
+
+        public static int countCode;
+        public static bool codeWrite;
+        int countTime;
+        DispatcherTimer disTimer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+            disTimer.Interval = new TimeSpan(0, 0, 1);
+            disTimer.Tick += new EventHandler(Timer);
         }
 
         private void BtnAutorizate_Click(object sender, RoutedEventArgs e)
@@ -33,21 +63,57 @@ namespace Authentication
                 MessageBox.Show("Заполните поле пароля");
             else
             {
-                if (tbLogin.Text == "adm")
+                if (tbLogin.Text == "ADM")
                 {
-                    if (pbPass.Password == "adm")
-                        ShowAuthorization();
+                    if (pbPass.Password == "ADM")
+                        Authorization();
                     else
-                        MessageBox.Show("Пользователь с таким логиным и паролем не найден");
+                        MessageBox.Show("Пароль не верный");
                 }
                 else
                     MessageBox.Show("Пользователь с таким логиным не найден");
             }
         }
 
-        private void ShowAuthorization()
+        private void Timer(object sender, EventArgs e)
         {
+            if (countTime == 0)
+            {
+                BtnAutorizate.Visibility = Visibility.Visible;
+                disTimer.Stop();
+                tbLogin.IsEnabled = true;
+                pbPass.IsEnabled = true;
+                tbNewCode.Visibility = Visibility.Collapsed;
+            }
+            else
+                tbNewCode.Text = "Получить новый код можно будет через " + countTime + " секунд";
+            countTime--;
+        }
 
+        private void Authorization()
+        {
+            codeWrite = false;
+            Random r = new Random();
+            int code = r.Next(0, 100000);
+            MessageBox.Show("Код для входа: " + code.ToString("D5") + "\nЗапомните его");
+            Code CodeT = new Code(code.ToString("D5"));
+            CodeT.ShowDialog();
+            if (codeWrite == true)
+            {
+
+            }
+            else 
+            {
+                if (countCode == 5)
+                    MessageBox.Show("Неверный код");
+                BtnAutorizate.Visibility = Visibility.Collapsed;
+                tbLogin.IsEnabled = false;
+                pbPass.IsEnabled = false;
+                countTime = 60;
+                tbNewCode.Text = "Получить новый код можно будет через " + countTime + " секунд";
+                tbNewCode.Visibility = Visibility.Visible;
+                disTimer.Start();
+            }
         }
     }
 }
